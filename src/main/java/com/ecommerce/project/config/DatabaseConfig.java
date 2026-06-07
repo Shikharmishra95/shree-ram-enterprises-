cf3cf56a5b9ae69216e70f71305cffe97c683c25
shikharmishra95 <shikharmishra9532@gmail.com>
2026-06-08 00:59:03 +0530

    fix: update DatabaseConfig to parse credentials and connection details from mysql:// URL

--- src/main/java/com/ecommerce/project/config/DatabaseConfig.java
+++ src/main/java/com/ecommerce/project/config/DatabaseConfig.java
@@ -1,35 +1,52 @@
 package com.ecommerce.project.config;
 
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
 import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Configuration
 public class DatabaseConfig {
 
     @Bean
     @Primary
-    @ConfigurationProperties("spring.datasource")
-    public DataSourceProperties dataSourceProperties() {
-        return new DataSourceProperties() {
-            @Override
-            public String getUrl() {
-                String url = super.getUrl();
-                if (url != null && url.startsWith("mysql://")) {
-                    return "jdbc:" + url;
-                }
-                return url;
-            }
-        };
-    }
-
-    @Bean
-    @Primary
     public DataSource dataSource(DataSourceProperties properties) {
+        String mysqlUrl = properties.getUrl();
+        if (mysqlUrl != null && mysqlUrl.startsWith("mysql://")) {
+            try {
+                URI uri = new URI(mysqlUrl);
+                String host = uri.getHost();
+                int port = uri.getPort();
+                String path = uri.getPath();
+                String userInfo = uri.getUserInfo();
+
+                String username = properties.getUsername();
+                String password = properties.getPassword();
+
+                if (userInfo != null && userInfo.contains(":")) {
+                    String[] parts = userInfo.split(":", 2);
+                    username = parts[0];
+                    password = parts[1];
+                }
+
+                String jdbcUrl = "jdbc:mysql://" + host + (port != -1 ? ":" + port : "") + path
+                        + "?createDatabaseIfNotExist=true&useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+
+                return DataSourceBuilder.create()
+                        .url(jdbcUrl)
+                        .username(username)
+                        .password(password)
+                        .driverClassName("com.mysql.cj.jdbc.Driver")
+                        .build();
+            } catch (URISyntaxException e) {
+                // Fallback to properties initialization
+            }
+        }
         return properties.initializeDataSourceBuilder().build();
     }
 }
